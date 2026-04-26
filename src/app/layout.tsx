931b1885a38f3ac463fc9e6f27c0ee7edf6a4fc7
Paulo Vieira <paulocvieirajunior@gmail.com>
2026-04-26 13:40:53 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", "font-sans", inter.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
